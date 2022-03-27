65854f3fcb0124361aeaef85cdc01e7f2e59257e
skyemin <1515054372@qq.com>
2022-03-27 13:43:50 +0800

    add note

--- bank-application/src/main/java/com/example/bank/application/TransferService.java
+++ bank-application/src/main/java/com/example/bank/application/TransferService.java
@@ -7,6 +7,7 @@ import com.example.bank.exception.InvalidCurrencyException;
 
 import java.math.BigDecimal;
 
+//应用层,服务编排
 public interface TransferService {
 
     Result<Boolean> transfer(Long sourceUserId, String targetAccountNumber, BigDecimal targetAmount, String targetCurrency) throws Exception, DailyLimitExceededException;
